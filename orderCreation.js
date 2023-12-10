2ccfaea8a32198a84aed64b95c71bff2c9844a53
RameeshaKhan <rameeshakhan75@gmail.com>
2023-12-10 15:59:11 +0500

    Code Formatted

--- orderCreation.js
+++ orderCreation.js
@@ -1,46 +1,43 @@
-// orderCreation.js
 const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid'); 
+const { v4: uuidv4 } = require('uuid');
 const sqs = new AWS.SQS();
 const lambda = new AWS.Lambda();
 require('dotenv').config();
 
 const orderCreationHandler = async (event) => {
-  try {
     const { productName, quantity, price, status = 'pending' } = JSON.parse(event.body);
+    try {
+        const order = {
+            orderId: uuidv4(),
+            productName,
+            quantity,
+            price,
+            status,
+        };
 
-    const order = {
-      orderId: uuidv4(),
-      productName,
-      quantity,
-      price,
-      status,
-    };
+        await sqs.sendMessage({
+            QueueUrl: process.env.QUEUE_URL,
+            MessageBody: JSON.stringify(order),
+        }).promise();
 
-    // Send the order to the SQS queue
-    await sqs.sendMessage({
-      QueueUrl: process.env.QUEUE_URL,
-      MessageBody: JSON.stringify(order),
-    }).promise();
+        await lambda.invoke({
+            FunctionName: 'Topic-dev-order-processing',
+            InvocationType: 'Event',
+        }).promise();
 
-    await lambda.invoke({
-      FunctionName: 'Topic-dev-order-processing', 
-      InvocationType: 'Event',
-    }).promise();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Order created successfully' }),
-    };
-  } catch (error) {
-    console.error('Error creating order:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
-    };
-  }
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Order created successfully, You will get notified when completed' }),
+        };
+    } catch (error) {
+        console.error('Error creating order:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
 };
 
 module.exports = {
-  handler: orderCreationHandler,
+    handler: orderCreationHandler,
 };
